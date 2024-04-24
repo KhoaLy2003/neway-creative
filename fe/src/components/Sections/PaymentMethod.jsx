fecc86912b1c3c16e3d4dbe1b0bd110ba5e59d11
Minh Dinh <macotontai123@gmail.com>
2024-04-24 19:06:10 +0700

    refactor(payment method): update Payment Method component

--- fe/src/components/Sections/PaymentMethod.jsx
+++ fe/src/components/Sections/PaymentMethod.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment, useState } from "react";
 
-function PaymentMethod({ options }) {
+function PaymentMethod() {
   const [selectedOption, setSelectedOption] = useState(null);
 
   const handleOptionChange = (option) => {
@@ -11,18 +11,25 @@ function PaymentMethod({ options }) {
     <Fragment>
       <h3>Payment method</h3>
 
-      <div className="payment-methods">
-        {options.map((option) => (
-          <label key={option.value} className="payment-option">
-            <input
-              type="radio"
-              value={option.value}
-              checked={selectedOption === option}
-              onChange={() => handleOptionChange(option)}
-            />
-            <img src={option.imageSrc} alt={option.label} />
-          </label>
-        ))}
+      <div className="payment-method">
+        <label className="image-radio">
+          <input
+            type="radio"
+            value="option1"
+            checked={selectedOption === "option1"}
+            onChange={() => handleOptionChange("option1")}
+          />
+          <img src="image1.png" alt="Option 1" />
+        </label>
+        <label className="image-radio">
+          <input
+            type="radio"
+            value="option2"
+            checked={selectedOption === "option2"}
+            onChange={() => handleOptionChange("option2")}
+          />
+          <img src="image2.png" alt="Option 2" />
+        </label>
       </div>
     </Fragment>
   );
--- fe/src/pages/PaymentPage.jsx
+++ fe/src/pages/PaymentPage.jsx
@@ -1,22 +1,17 @@
-import { React, Fragment } from "react";
+import { React, Fragment, useState } from "react";
 
 import PageHeading from "../components/Layouts/PageHeading";
 import About from "../components/Layouts/About";
 import PaymentMethod from "../components/Sections/PaymentMethod";
 
 function PaymentPage() {
-  const options = [
-    { value: "momo", label: "Momo", imageSrc: "../assets/momo.png" },
-    { value: "vn-pay", label: "VN Pay", imageSrc: "../assets/vn-pay.png" },
-  ];
-
   return (
     <Fragment>
       <PageHeading />
 
       <div className="container">
         <div className="col-md-6">
-          <PaymentMethod options={options} />
+          <PaymentMethod />
         </div>
       </div>
 
