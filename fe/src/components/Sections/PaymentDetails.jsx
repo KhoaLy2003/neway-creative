97fbd7fe62eddd86b4ef53e6fd3e76b98ea74fa3
Minh Dinh <macotontai123@gmail.com>
2024-05-01 14:11:17 +0700

    feat(button): add button for payment details form

--- fe/src/components/Sections/PaymentDetails.jsx
+++ fe/src/components/Sections/PaymentDetails.jsx
@@ -1,11 +1,12 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
+import Button from "@mui/material/Button";
 
 function PaymentDetails() {
   return (
     <Fragment>
       <h3>Payment Details</h3>
 
-      <form>
+      <form method="POST">
         <input
           className="form-control mb-3"
           type="text"
@@ -27,6 +28,11 @@ function PaymentDetails() {
           placeholder="Enter Name on Card"
         />
         <input className="form-control mb-3" type="text" placeholder="CVV" />
+
+        <Button sx={{ marginRight: "3rem" }} variant="outlined">
+          Back
+        </Button>
+        <Button variant="contained">Confirm Payment</Button>
       </form>
     </Fragment>
   );
