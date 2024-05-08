4090f01c1c56be26ce2e9d710f267067311f3e8c
Minh Dinh <macotontai123@gmail.com>
2024-05-08 08:21:32 +0700

    feat(about us page, assets): add our team members section, add images in assets

--- fe/src/pages/customer/AboutUsPage.jsx
+++ fe/src/pages/customer/AboutUsPage.jsx
@@ -3,6 +3,12 @@ import PageHeading from "../../components/Layouts/PageHeading";
 import aboutUsHeading from "../../assets/about-us-main.jpg";
 import aboutUs1 from "../../assets/about-us-1.jpg";
 import aboutUs2 from "../../assets/about-us-2.jpg";
+import teamMember1 from "../../assets/team-member-1.jpg";
+import teamMember2 from "../../assets/team-member-2.jpg";
+import teamMember3 from "../../assets/team-member-3.jpg";
+import teamMember4 from "../../assets/team-member-4.jpg";
+import teamMember5 from "../../assets/team-member-5.jpg";
+import teamMember6 from "../../assets/team-member-6.jpg";
 import "./AboutUsPage.css";
 
 const AboutUsPage = () => {
@@ -18,7 +24,7 @@ const AboutUsPage = () => {
         <div className="vision mb-5">
           <div className="row">
             <div className="col-md-6">
-              <h4>Vision</h4>
+              <h4 className="mb-5">Vision</h4>
 
               <h5>What is our vision ?</h5>
               <p>
@@ -37,7 +43,7 @@ const AboutUsPage = () => {
         <div className="mission mb-5">
           <div className="row">
             <div className="col-md-6">
-              <h4>Mission</h4>
+              <h4 className="mb-5">Mission</h4>
 
               <h5>What is our mission ?</h5>
               <p>
@@ -55,6 +61,58 @@ const AboutUsPage = () => {
             </div>
             <div className="col-md-6">
               <img src={aboutUs2} alt="About Us 2" className="img-fluid" />
+            </div>
+          </div>
+        </div>
+
+        <div className="team-members mb-5">
+          <h4 className="mb-5">Our Team Members</h4>
+
+          <div className="row">
+            <div className="col-md-4">
+              <img
+                src={teamMember1}
+                alt="1st Team Member"
+                className="img-fluid"
+              />
+            </div>
+            <div className="col-md-4">
+              <img
+                src={teamMember2}
+                alt="2nd Team Member"
+                className="img-fluid"
+              />
+            </div>
+            <div className="col-md-4">
+              <img
+                src={teamMember3}
+                alt="3rd Team Member"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-4">
+              <img
+                src={teamMember4}
+                alt="4th Team Member"
+                className="img-fluid"
+              />
+            </div>
+            <div className="col-md-4">
+              <img
+                src={teamMember5}
+                alt="5th Team Member"
+                className="img-fluid"
+              />
+            </div>
+            <div className="col-md-4">
+              <img
+                src={teamMember6}
+                alt="6th Team Member"
+                className="img-fluid"
+              />
             </div>
           </div>
         </div>
