fd84b87c687de71fc3bb69ecf680ca2709888943
khoalnd <khoaly090141@gmail.com>
2024-07-14 08:34:46 +0700

    update init sql script

---
+++ be/src/main/resources/db/migration/V3__Init_CalendarImage.sql
@@ -0,0 +1,13 @@
+INSERT INTO calendar_image (image_url,calendar_id) VALUES
+	 ('https://res.cloudinary.com/df75ybox6/image/upload/v1719032381/ideasy/Basic/xgs6npydt4i4xbelxg0z.png',1),
+	 ('https://res.cloudinary.com/df75ybox6/image/upload/v1719032380/ideasy/Basic/bqoiemddexhwnkrmulwp.png',1),
+	 ('https://res.cloudinary.com/df75ybox6/image/upload/v1719032380/ideasy/Basic/ovil6e9k6yjqtmjvtx0q.png',1),
+	 ('https://res.cloudinary.com/df75ybox6/image/upload/v1719032549/ideasy/Cafe/rzsmnblsucjsybjlh7ij.png',2),
+	 ('https://res.cloudinary.com/df75ybox6/image/upload/v1719032549/ideasy/Cafe/fpczvo9plpbumtbmw6yj.png',2),
+	 ('https://res.cloudinary.com/df75ybox6/image/upload/v1719032548/ideasy/Cafe/fno25xgndu9dgumgkahv.png',2),
+	 ('https://res.cloudinary.com/df75ybox6/image/upload/v1719032744/ideasy/Yoga/tm3czr8lm2haqxhexyk2.png',3),
+	 ('https://res.cloudinary.com/df75ybox6/image/upload/v1719032744/ideasy/Yoga/efazdf98tqtilgtz4v0j.png',3),
+	 ('https://res.cloudinary.com/df75ybox6/image/upload/v1719032745/ideasy/Yoga/jeueldsautx616kz4tkn.png',3),
+	 ('https://res.cloudinary.com/df75ybox6/image/upload/v1719032736/ideasy/Pet/fiegl3rwn8curjpitxsg.png',4),
+	 ('https://res.cloudinary.com/df75ybox6/image/upload/v1719032735/ideasy/Pet/vdmwmtt6ggibzqnzpyqp.png',4),
+	 ('https://res.cloudinary.com/df75ybox6/image/upload/v1719032735/ideasy/Pet/wpony2qqsxl8sdtjawps.png',4);
